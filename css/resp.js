786af471ed60a96181cb9f5374ec5e3d38440402
Suryansh Ruhal <suryanshruhal@gmail.com>
2023-06-23 14:08:32 +0530

    addcourosel

--- css/resp.js
+++ css/resp.js
@@ -1,3 +1,11 @@
+var counter=1;
+setInterval(function(){
+    document.getElementById('radio'+counter).checked = true;
+    counter++;
+    if(counter>5){
+        counter=1;
+    }
+}, 5000);
 burger = document.querySelector('.burger');
 logo = document.querySelector('.logo');
 list = document.querySelector('.list');
