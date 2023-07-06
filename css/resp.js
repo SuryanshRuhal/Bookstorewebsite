3e1d4161bb6c8cb0986667ea31758ac389884d9e
Suryansh Ruhal <suryanshruhal@gmail.com>
2023-07-06 23:31:14 +0530

    message box active

--- css/resp.js
+++ css/resp.js
@@ -14,3 +14,28 @@ burger.addEventListener('click', ()=>{
         logo.classList.toggle('visible');
         list.classList.toggle('visible');
     })
+const form= document.querySelector("#form");
+const submit= document.querySelector("#submit");
+
+const publickey = "lCC-8ZAow-9SLeOBN";
+emailjs.init(publickey);
+form.addEventListener("submit",e=>{
+    e.preventDefault();
+    let info={
+        from_name: document.getElementById("name").value,
+        email_id: document.getElementById("mail").value,
+        phone_no: document.getElementById("phone").value,
+        message: document.getElementById("message").value
+    }
+    emailjs.send("service_a2kmg9w","template_naw6th7",info).then(function(res){
+        alert("SUCESSS!!!")
+        document.getElementById("name").value="";
+        document.getElementById("mail").value="";
+        document.getElementById("phone").value="";
+        document.getElementById("message").value="";
+
+        
+    }, (error)=>{
+        alert("FAILED!, please retry.")
+    })
+})
